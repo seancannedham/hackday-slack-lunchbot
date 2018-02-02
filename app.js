3a1af65f7e854990d72fe8a688a4fe7bfb81f84e
Sean Canham <sean.canham@adknown.com>
2018-02-02 14:05:17 -0500

    fixing app

--- app.js
+++ app.js
@@ -12,8 +12,20 @@ app.listen(port, function() {
   console.log('Listenting on port ' + port);
 });
 
-app.post(port, function(req, res, next)
+app.post('/hello', function(req, res, next)
 {
+  var username = req.body.user_name;
+//  var option = getVotedOption(text); //not sure what the format of text is, defer impl this fn
+
+  var botPayLoad = {
+    text: 'Hello ' + username;
+  }
   //var botPayLoad will be set here
   //return res.status(200).json(botPayLoad)
+  if (username !== 'slackbot'){
+    return res.status(200).json(botPayLoad);
+  } else {
+    return res.status(200).end();
+  }
+  }
 });
