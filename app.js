3c780c3d18be3ca331b0c4224236029a6f796880
Sean Canham <sean.canham@adknown.com>
2018-02-02 15:47:00 -0500

    hardcoding is good too

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 1337;
 
+var worksvotes =0;
+var royalelectricvotes=0;
+var buongustovotes =0;
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {res.status(200).send('Hello World!'); });
@@ -17,9 +21,22 @@ app.post('/lunch', function(req, res, next)
   var input = req.body.text;
   var username = req.body.user_name;
 //  var option = getVotedOption(text); //not sure what the format of text is, defer impl this fn
+input = input.lower();
+
+if(input == 'works'){
+  worksvotes++;
+} else if(input == 'buon gusto'){
+  buongustovotes++;
+} else {
+  royalelectricvotes++;
+}
+
+var output = "works - " + worksvotes + '\n'
+              + "buon gusto - " + buongustovotes + '\n'
+              + "royal electric - " + royalelectricvotes;
 
   var botPayLoad = {
-    text: input
+    text: output
   };
   //var botPayLoad will be set here
   //return res.status(200).json(botPayLoad)
