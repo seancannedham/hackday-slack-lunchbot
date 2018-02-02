073686d26d2cafa49d2b587b10f830dd6b22be00
R9MWLN0-ALEXAND\Alexander Jakel <alex.i.jakel@gmail.com>
2018-02-02 16:56:33 -0500

    t

---
+++ interpreter.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Alexander Jakel on 2/2/2018.
+ */
+
+//takes in the input and determines which command needs to be run.
+function interpreter (user, command, data)
+{
+    switch (command)
+    {
+        case "/vote":
+            //call vote function
+            break;
+        case "/results":
+            //show all results
+            break;
+    }
+}
+
+function addVote(user, vote)
+{
+    //check user has not voted twice
+    //check location is valid
+
+
+}
